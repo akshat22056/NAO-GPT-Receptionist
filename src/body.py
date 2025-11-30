bd66e99ac132793a490acfef9b1ece811963004d
dkantrooiiitd <133583942+dkantrooiiitd@users.noreply.github.com>
2025-11-30 21:47:02 +0530

    Update body.py

--- src/body.py
+++ src/body.py
@@ -1,11 +1,7 @@
-from flask import Flask, request, jsonify,send_file
-from naoqi import ALProxy, ALModule, ALBroker
-import time
-from threading import Event
-from qi import Session,Application
-import os
+from flask import Flask, request, jsonify
+from naoqi import ALProxy, ALBroker
+from qi import Session
 import threading
-import time
 
 #Flask app setup and nao connection variables
 app = Flask(__name__)
@@ -17,18 +13,12 @@ sleep_time = 0.01
 
 tts = ALProxy("ALTextToSpeech", nao_IP, nao_port)
 tts.setVolume(1.0) # define volume of the robot
-animatedSpeech = ALProxy("ALAnimatedSpeech", nao_IP, nao_port)
-
-word_detected_event = Event()
-wake_word_detected = None
-partsTouched = ""
 
 # Custom functionalities that wrap naoqi behaviour/speaker modules to define behaviour
 class behavior:
     def __init__(self,session):
         self.name = ""
         self.behavior_mng_service = session.service("ALBehaviorManager")
-        self.motion_service = session.service("ALMotion")
         self.posture_service = session.service("ALRobotPosture")
         self.running_flag = False
 
@@ -42,7 +32,7 @@ class behavior:
         finally:
             self.running_flag = False
 
-    # NEW: generic launcher used by /run_behavior
+    # generic launcher used by /run_behavior
     def launch_behavior(self, behavior_name, async_run=True):
         """
         Launch any NAO behavior by its full name/path.
@@ -59,7 +49,7 @@ class behavior:
         else:
             self._run_behavior_blocking(behavior_name)
 
-    # NEW: hand-waving gesture using built-in NAO animations
+    # hand-waving gesture using built-in NAO animations
     def wave_hand(self, hand="right"):
         """
         Trigger a hand-waving gesture. Uses built-in gesture behaviors.
@@ -79,59 +69,20 @@ class behavior:
 
         self.launch_behavior(behavior_name, async_run=True)
     
-#this class initializes the speaker and also plays the files when asked to played 
-class SPEAKER:
-    def __init__(self,session):
-        self.aup = session.service("ALAudioPlayer")
-        self.path = "/home/nao/share/nao_gpt/"
-        self.buffer = []
-        self.sig_play = True
-        self.thread_alive = threading.Thread(target=self._thread_play,args=())
-
-    def append(self,filename):
-        self.sig_play = True
-        self.buffer.append(self.aup.loadFile(os.path.join(self.path,filename)))
-        
-    def _thread_play(self):
-        while len(self.buffer)!=0:
-            if self.sig_play:
-                self.aup.play(self.buffer.pop(0))
-            else: break
-    
-    def play(self):
-        if not self.thread_alive.is_alive():
-            playing = threading.Thread(target=self._thread_play,args=())
-            self.thread_alive = playing
-            playing.start()
-            
-            return "playing the file"
-        else:
-            return "there are some files in the play queue"
-    
-    def stop(self):
-        self.aup.stopAll()
-        self.aup.unloadAllFiles()
-        self.sig_play = False
-    
-    def isPlaying(self):
-        return self.thread_alive.is_alive()
-
 
 # Establish connection with robot using broker/naoqi session aided by ALproxy for broker
 try:
-    pythonBroker = ALBroker("pythonBroker", "0.0.0.0", 0, nao_IP, nao_port) # broker connection: essential for communicating between the module and the NAOqi runtime
+    pythonBroker = ALBroker("pythonBroker", "0.0.0.0", 0, nao_IP, nao_port) # broker connection
     global AudioCapture
     session = Session()
     session.connect("tcp://" + nao_IP + ":" + str(nao_port))
-    speaker = SPEAKER(session)
     behave = behavior(session)
 
 except RuntimeError:
     print("Error initializing broker!")
     exit(1)
 
-
-# server endpoints that utilize custom functions defined above to 
+# server endpoints that utilize custom functions defined above 
 
 @app.route("/talk", methods=["POST"])
 def talk():
